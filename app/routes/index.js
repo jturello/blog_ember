badddc6d2db0778e0c17822d4066888e194a423a
Christine Tacha and James Turello <jturello@yahoo.com>
2016-08-17 15:08:57 -0700

    add css and template for app

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
+var places = [{
+  id: 1,
+  location: "Honolulu, HI",
+  description: "Beautiful beaches, wonderful weather, and sharks don't eat people that often. Come visit!"
+  image: "http://paradiseintheworld.com/wp-content/uploads/2012/03/honolulu-beach.jpg"
+}, {
+  id: 2,
+  location: "Mytilene, Greece",
+  description: "Ancient city lovely beaches, day trips to Turkey, and very few shark attacks. Come visit!",
+  image: "http://www.mvmtravel.com/images/articleimages/2626/dimage2682.jpg"
+}, {
+  id: 3,
+  location: "Phuket, Thailand",
+  description: "Surrounded by water, beautiful beaches, sea caves and more shark attacks that other places, but they are small sharks",
+  image: "http://www.merittravel.com/wp-content/uploads/2014/08/Adv-Asia-Thailand-Thai-Sailling-960.jpg"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return places;
+  },
 });
