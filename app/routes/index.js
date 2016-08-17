e28b5013a88518ee6fe8ed8ce86604f16d6e1948
Christine Tacha and James Turello <tacha.christine@gmail.com>
2016-08-17 16:09:08 -0700

    fix minor typos

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
-var places = [{
+var destinations = [{
   id: 1,
   location: "Honolulu, HI",
-  description: "Beautiful beaches, wonderful weather, and sharks don't eat people that often. Come visit!"
+  description: "Beautiful beaches, wonderful weather, and sharks don't eat people that often. Come visit!",
   image: "http://paradiseintheworld.com/wp-content/uploads/2012/03/honolulu-beach.jpg"
 }, {
   id: 2,
@@ -13,12 +13,12 @@ var places = [{
 }, {
   id: 3,
   location: "Phuket, Thailand",
-  description: "Surrounded by water, beautiful beaches, sea caves and more shark attacks that other places, but they are small sharks",
+  description: "Surrounded by water, beautiful beaches, sea caves and more shark attacks than other places, but they are small sharks",
   image: "http://www.merittravel.com/wp-content/uploads/2014/08/Adv-Asia-Thailand-Thai-Sailling-960.jpg"
 }];
 
 export default Ember.Route.extend({
   model() {
-    return places;
+    return destinations;
   },
 });
